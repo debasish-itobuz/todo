d7cfefefcc3e8d99f7c471c007a215ba7f4feafa
debasish-itobuz <debasish.das@itobuz.com>
2025-04-21 11:54:21 +0530

    project started

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <h1 className="text-2xl font-bold text-center mt-6 text-red-500">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
